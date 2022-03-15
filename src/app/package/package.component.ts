ed4d86e360202fa53211a69dd811b183051c2c79
Anjithapushparaj <anjitha.pushparaj@gmail.com>
2022-03-15 10:46:24 +0530

    onChange() added

--- src/app/package/package.component.ts
+++ src/app/package/package.component.ts
@@ -11,12 +11,17 @@ export class PackageComponent implements OnInit {
   myName;
   employeeName = [];
   empName;
+  test = 3;
   constructor( private pkgService: PackageService){}
   ngOnInit(): void {
-    this.getPackages();
+    // this.getPackages();
     this.getName();
     this.getEmpName();
+    this.fetchPack();
+    // this.getNewPackages();
   }
+  // getNewPackages(){
+  // }
   getName(){
     this.pkgService.getMyname().subscribe((name)=>{ 
       this.myName = name;
@@ -36,8 +41,25 @@ export class PackageComponent implements OnInit {
     // asychronous call     
     this.pkgService.fetchPackages().subscribe((pkgData) => {
       this.packages = pkgData;
-      console.log(pkgData);
+      setTimeout(() => { 
+        this.packages=[{
+      
+          pkdName:'Wayanad Holiday',
+          pkgDesc:'' ,
+          destination: 'Wayanad',
+          subLocations: ['1', '2', '3'],
+          tripDays: 3,
+          travelOptions: ['Bus', 'Train', 'Car'],
+          foodOptions: ['Breakfast', 'Lunch', 'Dinner'],
+          price: 20000
+        }];
+      },2000);
     });
   }
-  onCreatePackage(){}
+  onCreatePackage(){
+    this.test += 1;
+  }
+  fetchPack(){
+    this.getPackages();
+  }
 }
