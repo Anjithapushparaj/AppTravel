6c44efe274748ae27acbbb8552551456ffefcc34
Anjithapushparaj <anjitha.pushparaj@gmail.com>
2022-03-17 10:54:15 +0530

    17Mar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  packages1 = {
+    name: 'Anju',
+    age:23
+  };
   title = 'travelManage';
+
+  constructor(){
+    // Object assign
+    this.packages1 = Object.assign(this.packages1, {lastname: 'Thampi'})
+    console.log('Packages1',this.packages1);
+    const NewPack = Object.assign({}, this.packages1, {location: 'PTA'});
+    console.log('NewPack:',NewPack)
+  }
 }
